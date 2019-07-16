a1df106b68fff990213ae8517057eb00d3168493
Mishanea <godzzilamd@mail.ru>
2019-07-16 16:20:45 +0300

    here

--- database/seeds/Permissions_rolesTableSeeder.php
+++ database/seeds/Permissions_rolesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Role;
 
 class Permissions_rolesTableSeeder extends Seeder
 {
@@ -11,6 +12,9 @@ class Permissions_rolesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $roles = Role::find(1);
+        $roles->permissions()->attach([1,2,3,4,5]);
+        $roles = Role::find(2);
+        $roles->permissions()->attach(1);
     }
 }
