650ae9a66ca53ad87b97a4e05c37447e1b3bbdc6
Mishanea <godzzilamd@mail.ru>
2019-07-18 15:17:11 +0300

    here

--- resources/views/sections/edit.blade.php
+++ resources/views/sections/edit.blade.php
@@ -1,49 +1,24 @@
 @extends('layouts.app')
 
-@section('subheader')
-    <div class="d-flex" style="background-color:#33334d">
-        <div class="col-md-4">
-            <div class="ml-3 text-light mt-1">
-                <a>{{ $category->title }}</a>
-                <span>></span>
-                <span>{{ $title }}</span>
-            </div>
-        </div>
-        <div class="col-md-8 mr-2" align='right'>
-            <a href="" class="btn btn-warning mr-1">New Category</a>
-            <a hef="" class="btn btn-warning" >New Section</a>
-        </div>
-    </div>
-@endsection
-
 @section('content')
     <div class="container mt-4">
-        <div>
-            @if (count($children) > 0)
-                <div class="text-light">Subsections</div>
-                @foreach ($children as $child)
-                    <div class="bg-light rounded mx-2 my-2 px-2 py-1">
-                        <div>{{ $child->title }}</div>
-                    </div>
-                @endforeach
-            @endif
-            @if (count($topics) > 0)
-                <div class="text-light">Topics</div>
-                @foreach($topics as $topic)
-                    <div class="bg-light rounded ml-5 mr-2 my-2 px-2 py-1">
-                        {{ $topic->title }}
-                    </div>
-                @endforeach
-            @else
-                <div class="text-light">No topics found</div>
-            @endif
+        <div class="container">
+            {!! Form::open(['action' => 'CategoryController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
+            <div class="form-group">
+                {{Form::label('category_id', 'Category', ['class' => 'text-light'])}}
+                {{Form::select('category_id', ['L' => 'Large', 'S' => 'Small'], null, ['placeholder' => 'Pick a category', 'class' => 'form-control'])}}
+                {{Form::label('parent_id', 'Parent', ['class' => 'text-light mt-2'])}}
+                {{Form::select('parent_id', ['L' => 'Large', 'S' => 'Small'], null, ['placeholder' => 'Pick a category', 'class' => 'form-control'])}}
+                {{Form::label('title', 'Title', ['class' => 'text-light mt-2'])}}
+                {{Form::text('title', "", ['class' => 'form-control'])}}
+                
+            </div>
+            </div>
         </div>
         <div class="d-flex mt-3">
-            <div class="col-md-6">
-            <a class="btn btn-primary" href="/section/{{ $id }}/edit">Edit</a>
-            </div>
             <div class="col-md-6" align='right'>
-                <a class="btn btn-danger" href="">Delete</a>
+                {{Form::submit('Submit', ['class'=>'btn btn-primary'])}}
+                {!! Form::close() !!}
             </div>    
         </div>
     </div>
