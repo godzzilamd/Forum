6e39d58d2e1ce1d9f6cb3d07577c6e4089fc5056
dorin <dorinciobanu17@gmail.com>
2019-07-22 12:02:17 +0300

    resr

--- resources/views/sections/show.blade.php
+++ resources/views/sections/show.blade.php
@@ -10,9 +10,11 @@
                 <a>{{ $section->title }}</a>
             </div>
         </div>
-        <div class="col-md-8 mr-2" align='right'>
-            <a href="" class="btn btn-warning m-1">New Section</a>
-        </div>
+        @if (Auth::user() && Auth::user()->hasPermission(14))
+            <div class="col-md-8 mr-2" align='right'>
+                <a href="" class="btn btn-warning m-1">New Section</a>
+            </div>
+        @endif
     </div>
 @endsection
 
@@ -26,7 +28,11 @@
                         <img src="{{$child->avatar}}" alt="" class="mr-2 rounded">
                     </div>
                     <div>
-                        <a class="card-link text-dark" href="/section/{{$child->id}}/edit"><h6 class="border-bottom border-gray pb-2 mb-0">{{$child->title}}</h6></a>
+                        @if(Auth::user() && Auth::user()->hasPermission(18))
+                            <a class="card-link text-dark" href="/section/{{$child->id}}/edit"><h6 class="border-bottom border-gray pb-2 mb-0">{{$child->title}}</h6></a>
+                        @else
+                            <h6 class="border-bottom border-gray pb-2 mb-0">{{$child->title}}</h6>
+                        @endif
                     </div>
                 </div>
                 @foreach($child->topics as $topic)
@@ -42,13 +48,34 @@
                 @endforeach
             </div>
         @endforeach
-        <div class="d-flex mt-3 pb-3">
-            <div class="col-md-6">
-                <a class="btn btn-primary" href="/section/{{ $section->id }}/edit">Edit</a>
+        @if (count($section->topics) > 0)
+            <div class="my-3 p-3 bg-white rounded shadow-sm">
+                <h6>Topics</h6>
+                @foreach($section->topics as $topic)
+                    @if($topic->post_it)
+                        <div class="media text-muted pt-3">
+                            {{--                            <img src="{{$topic->}}" alt="" class="mr-2 rounded">--}}
+                            <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                                <a class="text-dark" style="text-decoration: none" href="/topic/{{$topic->id}}"><strong class="d-block text-gray-dark">{{$topic->title}}</strong></a>
+                                Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                            </p>
+                        </div>
+                    @endif
+                @endforeach
             </div>
-            <div class="col-md-6" align='right'>
-                <a class="btn btn-danger" href="">Delete</a>
+        @endif
+            <div class="d-flex mt-3 pb-3">
+                @if (Auth::user() && Auth::user()->hasPermission(13))
+                    <div class="col-md-6">
+                        <a class="btn btn-primary" href="/section/{{ $section->id }}/edit">Edit</a>
+                    </div>
+                @endif
+                @if (Auth::user() && Auth::user()->hasPermission(15))
+                    <div class="col-md-6" align='right'>
+                        <a class="btn btn-danger" href="">Delete</a>
+                    </div>
+                @endif
+
             </div>
-        </div>
     </div>
 @endsection
