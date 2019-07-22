2472ad1b90fcec616cd171f52423dcde63b6cd83
dorin <dorinciobanu17@gmail.com>
2019-07-22 17:47:12 +0300

    resr

--- database/migrations/2019_07_15_3_create_topics_table.php
+++ database/migrations/2019_07_15_3_create_topics_table.php
@@ -16,8 +16,8 @@ class CreateTopicsTable extends Migration
         Schema::create('topics', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('section_id');
-            $table->string('title');
             $table->text('body');
+            $table->string('title');
             $table->boolean('post_it')->default(0);
             $table->boolean('closed')->default(0);
             $table->softDeletes();
