a0932370b9c39041382ff0c389d80899e7c4798d
dorin <dorinciobanu17@gmail.com>
2019-07-23 15:47:35 +0300

    resr

--- resources/views/category/edit.blade.php
+++ resources/views/category/edit.blade.php
@@ -1,24 +1,19 @@
 @extends('layouts.app')
 
 @section('subheader')
-<div class="d-flex bg-white shadow-sm">
-    <div class="col-md-4">
-        <div class="ml-3 mt-2">
-            <a>{{ $category->title }}</a>
+    <div class="d-flex bg-white shadow-sm">
+        <div class="col-md-4">
+            <div class="ml-3 mt-2">
+                <a href="/category/{{$category->id}}">{{ $category->title }}</a>
+            </div>
         </div>
     </div>
-    @if (Auth::user()->hasPermission(21))
-    <div class="col-md-8 mr-2" align='right'>
-            <a href="" class="btn btn-warning m-1">New Category</a>
-        </div>
-    @endif
-</div>
 @endsection
 
 @section('content')
-<div class="container">
-    @csrf
-    {!! Form::open(['action' => ['CategoryController@update', $category], 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
+    <div class="container">
+        @csrf
+        {!! Form::open(['action' => ['CategoryController@update', $category], 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
         <div class="form-group">
             {{Form::label('title', 'Title')}}
             {{Form::text('title', $category->title, ['class' => 'form-control', 'placeholder' => 'Name'])}}
@@ -26,25 +21,26 @@
         <label class="control-label" for="name">Incarca o imagine</label>
         <input id="file-5" name="photo" class="file" type="file" multiple>
         <div class="form-check">
-            <input class="form-check-input" name="isStaff" type="checkbox" id="gridCheck1" {{$category->isStaff ? "checked" : ""}}>
+            <input class="form-check-input" name="isStaff" type="checkbox"
+                   id="gridCheck1" {{$category->isStaff ? "checked" : ""}}>
             <label class="form-check-label" for="gridCheck1">
                 is staff
             </label>
         </div>
-    <div class="d-flex mt-3 pb-3">
-        <div class="col-md-6">
-            {{Form::hidden('_method','PUT')}}
-            {{Form::submit('Submit', ['class'=>'btn btn-primary'])}}
-            {!! Form::close() !!}
+        <div class="d-flex mt-3 pb-3">
+            <div class="col-md-6">
+                {{Form::hidden('_method','PUT')}}
+                {{Form::submit('Submit', ['class'=>'btn btn-primary'])}}
+                {!! Form::close() !!}
+            </div>
+            <div class="col-md-6" align='right'>
+                {!!Form::open(['action' => ['CategoryController@destroy', $category], 'method' => 'POST', 'class' => 'pull-right'])!!}
+                {{Form::hidden('_method', 'DELETE')}}
+                {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+                {!!Form::close()!!}
+            </div>
         </div>
-        <div class="col-md-6" align='right'>
-            {!!Form::open(['action' => ['CategoryController@destroy', $category], 'method' => 'POST', 'class' => 'pull-right'])!!}
-            {{Form::hidden('_method', 'DELETE')}}
-            {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
-            {!!Form::close()!!}
-        </div>
+
+
     </div>
-
-
-</div>
 @endsection
