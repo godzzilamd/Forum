db66fb8df929d0e92d62e5c0b4e11b9ead356d42
dorin <dorinciobanu17@gmail.com>
2019-07-25 11:47:04 +0300

    resr

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -37,6 +37,61 @@ class PostsTableSeeder extends Seeder
                 'topic_id' => 1,
                 'body' => 'Al cincilea post',
             ],
+            [
+                'user_id' => 2,
+                'topic_id' => 2,
+                'body' => 'post topic Nissan X-Trail',
+            ],
+            [
+                'user_id' => 2,
+                'topic_id' => 3,
+                'body' => 'post topic Ford',
+            ],
+            [
+                'user_id' => 1,
+                'topic_id' => 4,
+                'body' => 'post topic Iphone Xr',
+            ],
+            [
+                'user_id' => 3,
+                'topic_id' => 5,
+                'body' => 'post topic Galaxy S25',
+            ],
+            [
+                'user_id' => 4,
+                'topic_id' => 6,
+                'body' => 'post topic Ferrari',
+            ],
+            [
+                'user_id' => 1,
+                'topic_id' => 7,
+                'body' => 'post topic Audi',
+            ],
+            [
+                'user_id' => 4,
+                'topic_id' => 8,
+                'body' => 'post topic Lambo',
+            ],
+            [
+                'user_id' => 3,
+                'topic_id' => 9,
+                'body' => 'post topic furi',
+            ],
+            [
+                'user_id' => 2,
+                'topic_id' => 10,
+                'body' => 'post topic microbuse',
+            ],
+            [
+                'user_id' => 2,
+                'topic_id' => 11,
+                'body' => 'post topic busuri',
+            ],
+            [
+                'user_id' => 1,
+                'topic_id' => 12,
+                'body' => 'post topic amioane mici',
+            ],
         ]);
     }
 }
