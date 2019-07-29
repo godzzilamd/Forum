9d48383a8ba7269f5f08ae7a6e8e229d15b59df8
Mishanea <godzzilamd@mail.ru>
2019-07-29 14:57:01 +0300

    here

--- app/Http/Controllers/ForumController.php
+++ app/Http/Controllers/ForumController.php
@@ -3,18 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Category;
-use App\Role;
-use App\User;
 
 class ForumController extends Controller
 {
 
     public function index()
     {
+        // $users = User::all();
         if (!auth()->user() || auth()->user()->role_id == '4')
             $categories = Category::where('isStaff', 1)->get();
         else
-            $categories = Category::with('sections')->get();
+            $categories = Category::with('parents')->get();
         return view('forms.view', compact(['categories']));
     }
 }
